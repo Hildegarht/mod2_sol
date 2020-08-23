7480ce8d59921f33f660f1981937889025a7dfa5
Hilda <hildaamadu@gmail.com>
2020-08-23 19:50:52 +0000

    New

--- mod4/js/SpeakHello.js
+++ mod4/js/SpeakHello.js
@@ -1,10 +1,9 @@
 
 (function(window){
 	var helloSpeaker = {};
- helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 var speakWord = "Hello";
-helloSpeaker.speak =function(names) {
-  console.log(speakWord + " " + helloSpeaker.names);
+helloSpeaker.speak =function(name) {
+  console.log(speakWord + " " + name);
 }
-window.helloSpeaker=helloSpeaker;
+window.helloSpeaker = helloSpeaker;
 })(window);
--- mod4/js/script.js
+++ mod4/js/script.js
@@ -1,11 +1,11 @@
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-window.names=names;
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 for (var i = 0; i < names.length; i++) {
-  console.log(speakWord + " " + name);
+  
    var firstLetter = names[i].charAt(0).toLowerCase();
+
     if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
@@ -13,4 +13,4 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})(window);
+})();
